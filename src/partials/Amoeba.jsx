666379ba7103fdc950add9b0c16dd042da42a9fb
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-04-30 18:53:50 +0530

    fix: Adjust About section height for improved layout and update Amoeba component positioning

--- src/partials/Amoeba.jsx
+++ src/partials/Amoeba.jsx
@@ -56,7 +56,7 @@ const Amoeba = () => {
   const pathData = generateSmoothPath(pointsRef.current, time);
 
   return (
-    <div className="flex justify-center items-center lg:h-[55vh] h-[40vh] mt-0 lg:mt-0">
+    <div className="flex justify-center items-center  lg:h-[55vh] h-[40vh] mt-0 lg:mt-0">
       <img className="z-9 absolute top-[25%] left-[30%] h-[30vh] lg:h-[35vh] " src="./profile.png" alt="svg" />
 
       <svg viewBox="0 0 400 400" width="500" height="500">
